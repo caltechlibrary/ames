e845ac9a45686506c711c47e31201bd4d0cd890b
Tom Morrell <tmorrell@caltech.edu>
2018-11-21 12:02:59 -0800

    Start of media improvements

--- matchers/update_datacite.py
+++ matchers/update_datacite.py
@@ -1,18 +1,31 @@
-import os,subprocess,json,re
+import os,subprocess,json
 import dataset
 import requests
+from datetime import date, datetime
 
 def update_datacite_media(username,password):
-    collection = 'media_records.ds'
+    collection = 'caltechdata.ds'
     keys = dataset.keys(collection)
+    result = dataset.has_key(collection,'mediaupdate')
+    if result == True:
+        update,err = dataset.read(collection,'mediaupdate')
+        update = date.fromisoformat(update)
+    else:
+        #Arbitrary old date - everythign will be updated
+        update = date(2011,1,1)
     for k in keys:
         print(k)
         existing,err = dataset.read(collection,k)
         if err !="":
             print(f"Unexpected error on read: {err}")
-        
+        print(existing['updated'])
+        record_update = datetime.fromisoformat(existing['updated']).date()
+        if record_update > update:
+            print("UPDATE")
         #Should have some sensible update check
         if 'electronic_location_and_access' in existing:
+            print("YES")
+            exit()
             for file_met in existing['electronic_location_and_access']:
                 if file_met['electronic_name'][0].split('.')[-1] == 'nc':
                     url = 'https://mds.datacite.org/media/'+existing['doi'] 
--- run_media_update.py
+++ run_media_update.py
@@ -1,9 +1,9 @@
 import os
-from harvesters import get_cd_media
 from matchers import update_datacite_media
 
 password = os.environ['DATACITE']
 
-get_cd_media(False)
+os.chdir('data')
+
 update_datacite_media('CALTECH.LIBRARY',password)
 
