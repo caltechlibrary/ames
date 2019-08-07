983722138e5e8fee354fc39ac397e274d2070a4f
Tom Morrell <tmorrell@caltech.edu>
2019-08-07 11:52:53 -0700

    Format

--- run_usage.py
+++ run_usage.py
@@ -13,7 +13,7 @@ os.chdir("data")
 production = True
 collection = "caltechdata.ds"
 
-#get_caltechdata(collection, production)
+# get_caltechdata(collection, production)
 
 mapping_file = "tindfile_mapping.csv"
 
@@ -45,9 +45,9 @@ if aggregate:
     build_aggregate(month_collection)
     aggregate_usage(usage_collection, month_collection)
 
-#keys = dataset.keys(month_collection)
-#keys.remove('2019-08')
-#keys = ['2018-07']
+# keys = dataset.keys(month_collection)
+# keys.remove('2019-08')
+# keys = ['2018-07']
 today = datetime.today()
 last_month = today.replace(day=1) - timedelta(days=1)
 keys = [f"{last_month.year}-{last_month.month:02}"]
