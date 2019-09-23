6b265609a05861108ef92a3944e1bfbcca23cc02
Tom Morrell <tmorrell@caltech.edu>
2019-09-23 12:07:37 -0700

    Add in video formats to mdeia updates

--- run_media_update.py
+++ run_media_update.py
@@ -1,13 +1,26 @@
 import os
+from datetime import date
+from py_dataset import dataset
 from ames.harvesters import get_caltechdata
 from ames.matchers import update_datacite_media
 
 password = os.environ["DATACITE"]
+prefix = "10.14291"
+user = "CALTECH.LIBRARY"
 
 os.chdir("data")
 
 collection = "caltechdata.ds"
-prefix = "10.14291"
 
 get_caltechdata(collection)
-update_datacite_media("CALTECH.LIBRARY", password, collection, prefix)
+update_datacite_media(user, password, collection, prefix)
+
+password = os.environ["TIND_DATACITE"]
+prefix = "10.22002"
+user = "TIND.CALTECH"
+
+update_datacite_media(user, password, collection, prefix)
+
+# Save date in collection
+today = date.today().isoformat()
+dataset.update(collection, "mediaupdate", {"mediaupdate": today})
