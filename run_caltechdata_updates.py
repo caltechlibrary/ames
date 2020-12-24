9597017e69f30de972d670dd9202f49eeb940f3d
Tom Morrell <tmorrell@caltech.edu>
2020-12-24 00:09:10 +0000

    Add update example

--- run_caltechdata_updates.py
+++ run_caltechdata_updates.py
@@ -2,6 +2,7 @@ import os
 from ames.harvesters import get_caltechdata
 from ames.matchers import add_citation
 from ames.matchers import update_citation
+from py_dataset import dataset
 
 if os.path.isdir("data") == False:
     os.mkdir("data")
@@ -14,3 +15,12 @@ collection = "caltechdata.ds"
 
 get_caltechdata(collection, production)
 add_citation(collection, token, production)
+
+#If we need to update
+#keys = dataset.keys(collection)
+#for k in keys:
+#    record, err = dataset.read(collection, k)
+#    if err != "":
+#        print(err)
+#        exit()
+#    update_citation(record, k, token, production)
