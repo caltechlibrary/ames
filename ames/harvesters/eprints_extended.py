ff0dcf948ce19fbd4f4bc1b65a0c174a0f1aa22d
Thomas Morrell <tmorrell@users.noreply.github.com>
2021-11-22 16:44:34 -0800

    Add upper and lowercase versions

--- ames/harvesters/eprints_extended.py
+++ ames/harvesters/eprints_extended.py
@@ -1,11 +1,18 @@
 import requests
 
+base_url = "http://localhost:8484/"
+
 
 def doi_in_authors(doi):
-    base_url = "http://localhost:8484/caltechauthors/doi/"
-    results = requests.get(base_url + doi).json()
-    print(results)
+    results = []
+    results += get_extended("caltechauthors", "doi", doi)
+    results += get_extended("caltechauthors", "doi", doi.lower())
+    results += get_extended("caltechauthors", "doi", doi.upper())
     if len(results) == 0:
         return False
     else:
         return True
+
+
+def get_extended(repository, typev, identifier):
+    return requests.get(base_url + f"{repository}/{typev}/{identifier}").json()
