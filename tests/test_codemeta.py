fdd6ff3f4accf5761c31bc8034bc9b38863c5acd
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-02-17 13:10:16 -0800

    Formatting and bump version

--- tests/test_codemeta.py
+++ tests/test_codemeta.py
@@ -2,16 +2,16 @@ import json
 from datacite import schema40
 from ames import codemeta_to_datacite
 
-infile = open('codemeta.json','r')
+infile = open("codemeta.json", "r")
 meta = json.load(infile)
 standardized = codemeta_to_datacite(meta)
-standardized['identifier'] = {'identifier':'10.1/1','identifierType':'DOI'}
-standardized['titles']= [{'title': "Title"}]
-standardized['publisher'] = 'publisher'
-standardized['publicationYear'] = '2018'
-standardized['resourceType'] = {'resourceTypeGeneral':"Software"}
-result =  schema40.validate(standardized)
-#Debugging if this fails
+standardized["identifier"] = {"identifier": "10.1/1", "identifierType": "DOI"}
+standardized["titles"] = [{"title": "Title"}]
+standardized["publisher"] = "publisher"
+standardized["publicationYear"] = "2018"
+standardized["resourceType"] = {"resourceTypeGeneral": "Software"}
+result = schema40.validate(standardized)
+# Debugging if this fails
 if result == False:
     v = schema40.validator.validate(standardized)
     errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
