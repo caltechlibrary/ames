d8c8d79c376c160c3d8903fcaad05143033cbf1e
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-08-22 13:53:41 -0700

    Fix queue harvesting

--- ames/harvesters/caltechauthors.py
+++ ames/harvesters/caltechauthors.py
@@ -1,8 +1,8 @@
-import requests
+import requests, math
 
 
-def get_pending_requests(token, community=None):
-    url = "https://authors.caltechlibrary.dev/api/requests"
+def get_pending_requests(token, community=None, return_ids=False):
+    url = "https://authors.caltechlibrary.dev/api/requests?q=is_open:true"
 
     headers = {
         "Authorization": "Bearer %s" % token,
@@ -10,11 +10,20 @@ def get_pending_requests(token, community=None):
     }
 
     if community:
-        url += "?receiver.community=" + community
-    r = requests.get(url, headers=headers)
-    data = r.json()
+        url += "%20AND%20receiver.community:" + community
+    response = requests.get(url, headers=headers)
+    total = response.json()["hits"]["total"]
+    pages = math.ceil(int(total) / 1000)
+    hits = []
+    for c in range(1, pages + 1):
+        chunkurl = f"{url}&size=1000&page={c}"
+        response = requests.get(chunkurl, headers=headers).json()
+        hits += response["hits"]["hits"]
+
     req = []
-    for item in data["hits"]["hits"]:
-        if item["is_open"] == True:
+    for item in hits:
+        if return_ids:
+            req.append(item["topic"]["record"])
+        else:
             req.append(item["id"])
     return req
