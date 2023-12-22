b9d6fc76835dd247ebd377009755d77f0fde1cfa
Tom Morrell <tmorrell@caltech.edu>
2023-12-22 11:50:46 -0800

    Full script for DOIs

--- add_doi_authors.py
+++ add_doi_authors.py
@@ -1,9 +1,13 @@
-import os, sys
+import os, sys, csv
 import requests
 from ames.matchers import add_doi
 
 token = os.environ["CTATOK"]
 
-record = sys.argv[1]
+record_file = sys.argv[1]
 
-add_doi(record, token)
+with open(record_file, "r") as f:
+    reader = csv.DictReader(f)
+    for record in reader:
+        if record["Publisher"] == "California Institute of Technology":
+            add_doi(record["Record ID"], token)
