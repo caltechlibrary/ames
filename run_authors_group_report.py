8cd53c1edc8fab57c3d5b960a442fd0637511121
Tom Morrell <tmorrell@caltech.edu>
2024-01-08 15:10:00 -0800

    Remove unneded token

--- run_authors_group_report.py
+++ run_authors_group_report.py
@@ -2,14 +2,12 @@ import os, sys, csv
 from ames.harvesters import get_group_records
 from ames.matchers import edit_author_identifier
 
-token = os.environ["CTATOK"]
-
 group_identifier = sys.argv[1]
 
 outfile = open(f"{group_identifier}_report.csv", "w")
 writer = csv.writer(outfile)
 
-to_update = get_group_records(token, group_identifier)
+to_update = get_group_records(group_identifier)
 for record in to_update:
     if "doi" not in record["pids"]:
         metadata = record["metadata"]
