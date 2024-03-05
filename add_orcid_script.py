d1102e7f2e41cd47c3963de465f5b8463ac0da73
Tom Morrell <tmorrell@caltech.edu>
2024-03-05 13:02:12 -0800

    Add orcid script

---
+++ add_orcid_script.py
@@ -0,0 +1,9 @@
+import csv,os
+
+with open('orcids.csv', 'r') as f:
+    reader = csv.reader(f)
+    orcid_list = list(reader)
+    for orcid_data in orcid_list:
+        orcid = orcid_data[8]
+        clpid = orcid_data[10]
+        os.system(f'python run_authors_name_update.py {clpid} {orcid} -add -new-scheme orcid')
