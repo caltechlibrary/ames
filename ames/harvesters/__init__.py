e88dae1b4de7cadffa815e323a89ab1e7b3ff62a
Tom Morrell <tmorrell@caltech.edu>
2025-07-30 10:53:51 -0700

    add missing import

--- ames/harvesters/__init__.py
+++ ames/harvesters/__init__.py
@@ -18,6 +18,7 @@ from .caltechauthors import get_request_comments
 from .caltechauthors import get_request_id_title
 from .caltechauthors import get_publisher
 from .caltechauthors import get_authors
+from .caltechauthors import get_doi
 from .caltechauthors import classify_link
 from .caltechauthors import extract_https_links
 from .caltechauthors import clean_link
